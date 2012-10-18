501e11df1adf4e462295d581adef57a56249868d
Daniel Rodriguez <df.rodriguez143@gmail.com>
2012-10-18 19:47:35 +0100

    PEP 8 Rules valid

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from distutils.core import setup
-import py2exe
+
 
 includes = ['wx']
 excludes = []
 packages = []
 dll_excludes = []
 data_files = [
-	('images', ['./images/task.ico']),
-	('images', ['./images/settings.png']),
-	('images', ['./images/settings.ico'])
+    ('images', ['./images/task.ico']),
+    ('images', ['./images/settings.png']),
+    ('images', ['./images/settings.ico'])
 ]
 
 setup(
-	data_files = data_files,
-	options = {"py2exe": {"compressed": 2,
-												"optimize": 2,
-												"includes": includes,
-												"excludes": excludes,
-												"packages": packages,
-												"dll_excludes": dll_excludes,
-												"bundle_files": 3,
-												"dist_dir": "dist",
-												"xref": False,
-												"skip_archive": False,
-												"ascii": False,
-												"custom_boot_script": '',
-											 }
-						},
-	zipfile = None,
-	windows=['newtask.py']
+    data_files=data_files,
+    options={"py2exe": {"compressed": 2,
+                                                "optimize": 2,
+                                                "includes": includes,
+                                                "excludes": excludes,
+                                                "packages": packages,
+                                                "dll_excludes": dll_excludes,
+                                                "bundle_files": 3,
+                                                "dist_dir": "dist",
+                                                "xref": False,
+                                                "skip_archive": False,
+                                                "ascii": False,
+                                                "custom_boot_script": '',
+                                             }
+                        },
+    zipfile=None,
+    windows=['newtask.py']
 )
