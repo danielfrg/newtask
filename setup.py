8757e5357b15fc795687d346d558902384a362d1
Daniel Rodriguez <df.rodriguez143@gmail.com>
2012-10-18 19:59:02 +0100

    PEP 8 Valid

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+import py2exe
 
 
 includes = ['wx']
@@ -13,21 +14,21 @@ data_files = [
 
 setup(
     data_files=data_files,
-    options={"py2exe": {
-                "compressed": 2,
-                "optimize": 2,
-                "includes": includes,
-                "excludes": excludes,
-                "packages": packages,
-                "dll_excludes": dll_excludes,
-                "bundle_files": 3,
-                "dist_dir": "dist",
-                "xref": False,
-                "skip_archive": False,
-                "ascii": False,
-                "custom_boot_script": '',
-                }
-            },
-    zipfile=None,
-    windows=['newtask.py']
+        options={"py2exe": {
+                        "compressed": 2,
+                        "optimize": 2,
+                        "includes": includes,
+                        "excludes": excludes,
+                        "packages": packages,
+                        "dll_excludes": dll_excludes,
+                        "bundle_files": 3,
+                        "dist_dir": "dist",
+                        "xref": False,
+                        "skip_archive": False,
+                        "ascii": False,
+                        "custom_boot_script": '',
+                    }
+                },
+        zipfile=None,
+        windows=['newtask.py']
 )
